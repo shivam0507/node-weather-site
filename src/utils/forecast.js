7e342892809d6b62e0664532959051618a804926
Shivam Gupta <guptashivam0507@gmail.com>
2022-03-19 18:29:13 +0530

    Updated forcast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, tempUnit, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=f8ef0a8c210668cd18da99de4874899b&query='+ latitude + ',' + longitude +'&units=' + tempUnit
+    const url = 'http://api.weatherstack.com/current?access_key=f8ef0a8c210668cd18da99de4874899b&query=' + latitude + ',' + longitude + '&units=' + tempUnit
 
-    request({url, json: true}, (error, {body} = {}) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to weather service', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'It is currently '+ body.current.temperature + ' degress out. It feels like '+ body.current.feelslike + ' degress out.')
+            callback(undefined, 'It is ' + body.current.weather_descriptions[0] + ' outside. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. Humidity: ' + body.current.humidity)
         }
     })
 }
